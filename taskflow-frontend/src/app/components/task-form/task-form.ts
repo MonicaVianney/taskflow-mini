e57bbbf6257ba967ecc8ce8d62c258d3c31eae57
MonicaVianney <monik.rocker15@gmail.com>
2025-11-19 15:25:04 -0600

    Fix CORS: Add OPTIONS method and improve configuration

--- taskflow-frontend/src/app/components/task-form/task-form.ts
+++ taskflow-frontend/src/app/components/task-form/task-form.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Task } from '../../services/task';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './task-form.html',
   styleUrl: './task-form.css',
 })
-export class TaskForm {
+export class TaskForm implements OnInit{
+  taskId: string | null = null;
+  title: string = '';
+  description: string = '';
+  completed: boolean = false;
+  isEditMode: boolean = false;
 
+  constructor(
+    private taskService: Task,
+    private router: Router,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+      this.taskId = this.route.snapshot.paramMap.get('id');
+      this.isEditMode = !!this.taskId;
+
+      if (this.isEditMode && this.taskId) {
+        this.loadTask(this.taskId);
+      }
+  }
+
+  loadTask(id: string): void {
+    this.taskService.getTaskById(id).subscribe({
+      next: (task) => {
+        this.title = task.title;
+        this.description = task.description;
+        this.completed = task.completed;
+      },
+      error: (error) => {
+        console.error('Error al cargar tarea', error);
+        this.router.navigate(['/tasks']);
+      }
+    });
+  }
+
+  onSubmit(): void {
+    const taskData = {
+      title: this.title,
+      description: this.description,
+      completed: this.completed
+    };
+
+    if (this.isEditMode && this.taskId) {
+      this.taskService.updateTask(this.taskId, taskData).subscribe({
+        next: () => {
+          this.router.navigate(['/tasks']);
+        },
+        error: (error) => {
+          console.error('Error al actualizar tarea', error);
+        }
+      });
+    } else {
+      this.taskService.createTask(taskData).subscribe({
+        next: () => {
+          this.router.navigate(['/tasks']);
+        },
+        error: (error) => {
+          console.error('Error al crear tarea', error);
+        }
+      });
+    }
+  }
+
+  cancel(): void {
+    this.router.navigate(['/tasks']);
+  }
 }
--- taskflow-frontend/src/app/components/task-list/task-list.ts
+++ taskflow-frontend/src/app/components/task-list/task-list.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Task } from '../../services/task';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
-export class TaskList {
+export class TaskList implements OnInit {
+  tasks: any[] = [];
+  loading: boolean = true;
 
+  constructor(
+    private taskService: Task,
+    private auth: Auth,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe({
+      next: (data) => {  
+        this.tasks = data;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error al cargar tareas', error);
+        this.loading = false;
+      }
+    });
+  }
+
+  createTask(): void {
+    this.router.navigate(['/tasks/new']);
+  }
+
+  editTask(id: string): void {
+    this.router.navigate(['/tasks/edit', id]);
+  }
+
+  toggleTask(id: string): void {
+    this.taskService.toggleTask(id).subscribe({
+      next: () => {
+        this.loadTasks();
+      },
+      error: (error) => {
+        console.error('Error al camhbiar estado:', error);
+      }
+    });
+  }
+
+  deleteTask(id: string): void {
+    if (confirm('¿Estás seguro de que deseas eliminar esta tarea?')) {
+      this.taskService.deleteTask(id).subscribe({
+        next: () => {
+          this.loadTasks();
+        },
+        error: (error) => {
+          console.error('Error al eliminar tarea:', error);
+        }
+      });
+    }
+  }
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
 }
